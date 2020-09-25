cb469add902a0d6314f001f6083b7274f6fc5802
Maxendre Martin <maxendremartin@gmail.com>
2020-09-25 16:21:44 +0200

    menu , taxonomies , organisation fichiers

--- Wordpress/premierWordpress/wp-content/themes/theme-maxendre/functions.php
+++ Wordpress/premierWordpress/wp-content/themes/theme-maxendre/functions.php
@@ -3,12 +3,16 @@
 function maxendre_supports()
 {
     add_theme_support('title-tag');
+    add_theme_support('post-thumbnails');
+    add_theme_support('menus');
+    register_nav_menu('header', 'En tête du menu');
+    register_nav_menu('footer', 'Pied de page');
 }
 
 function maxendre_register_assets()
 {
-    wp_register_style('bootstrap', 'https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css');
-    wp_register_script('bootstrap', 'https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js', ['popper', 'jquery']);
+    wp_register_style('bootstrap', 'https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css', []);
+    wp_register_script('bootstrap', 'https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js', ['popper', 'jquery'], false, true);
     wp_register_script('popper', 'https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js', [], false, true);
     wp_deregister_script('jquery');
     wp_register_script('jquery', 'https://code.jquery.com/jquery-3.5.1.slim.min.js', [], false, true);
@@ -16,5 +20,66 @@ function maxendre_register_assets()
     wp_enqueue_script('bootstrap');
 }
 
+function maxendre_separator()
+{
+    return '|';
+}
+
+function maxendre_menu_class($classes)
+{
+    $classes[] = 'nav-item';
+    return $classes;
+}
+
+function maxendre_menu_link_class($attrs)
+{
+    $attrs['class'] = 'nav-link';
+    return $attrs;
+}
+
+function maxendre_pagination()
+{
+    $pages = paginate_links(['type' => 'array']);
+    if ($pages === null) {
+        return;
+    }
+    echo '<nav aria-label="Pagination" class="my-4">';
+    echo '<ul class="pagination">';
+    foreach ($pages as $page) {
+        $active = strpos($page, 'current') !== false;
+        $class = 'page-item';
+        if ($active) {
+            $class .= 'active';
+        }
+        echo '<li class="' . $class . '">';
+        echo str_replace('page-numbers', 'page-link', $page);
+        echo '</li>';
+    }
+    echo '</ul>';
+    echo '</nav>';
+}
+
+function maxendre_init()
+{
+    register_taxonomy('music', 'post', [
+        'labels' => [
+            'name' => "Music",
+            'search_items' => "Rechercher des styles de musiques",
+            'all_items' => "Tous les styles de musique",
+            'edit_item' => "Editer le style de musique",
+            'update_item' => "Mettre à jour le style de musique",
+            'add_new_item' => "Ajouter un genre de musique",
+            'menu_name' => "Music",
+        ],
+        'show_in_rest' => true,
+        'hierarchical' => true,
+        'show_admin_column' => true,
+    ]);
+}
+
+add_action('init', 'maxendre_init');
 add_action('after_setup_theme', 'maxendre_supports');
 add_action('wp_enqueue_scripts', 'maxendre_register_assets');
+add_filter('document_title_separator', 'maxendre_separator');
+add_filter('nav_menu_css_class', 'maxendre_menu_class');
+add_filter('nav_menu_link_attributes', 'maxendre_menu_link_class');
--- Wordpress/premierWordpress/wp-content/themes/theme-maxendre/index.php
+++ Wordpress/premierWordpress/wp-content/themes/theme-maxendre/index.php
@@ -1,4 +1,33 @@
 <?php get_header() ?>
-boujour tout le monde
+
+<?php wp_list_categories(['taxonomy' => 'music', 'title_li' => '']); ?>
+
+<?php if (have_posts()) : ?>
+    <div class="row">
+        <?php while (have_posts()) : the_post(); ?>
+            <div class="col-sm-4">
+                <div class="card">
+                    <?php the_post_thumbnail('medium', ['class' => 'card-img-top', 'alt' => '', 'style' => 'height: auto;']) ?>
+                    <div class="card-body">
+                        <h5 class="card-title"><?php the_title() ?></h5>
+                        <ul>
+                            <?php
+                            the_terms(get_the_ID(), 'music', '<li>', '</li><li>', '</li>');
+                            ?>
+                            <p class="card-text"><?php the_content() ?></p>
+
+                    </div>
+                </div>
+            </div>
+        <?php endwhile ?>
+
+    </div>
+
+    <?php maxendre_pagination() ?>
+
+
+<?php else : ?>
+    <h1>Pas d'articles</h1>
+<?php endif; ?>
 
 <?php get_footer() ?>
