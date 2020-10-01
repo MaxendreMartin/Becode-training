8a85c0beaad3279d706516d01ae45ed3b00116a5
Maxendre Martin <maxendremartin@gmail.com>
2020-10-01 10:47:58 +0200

    gitignore

---
+++ NodeJs/node_first_app/index.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const app = express();
+require("./routes/userRoutes")(app);
+const PORT = 5000;
+app.listen(PORT, () => {
+    console.log("Server running");
+});
